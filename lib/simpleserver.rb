ae377642c5a961d2fcd8efe0122ba622853c9f8b
Alex Lien <codefellow@aqlien.com>
2014-06-10 15:43:02 -0700

    add removal of URL parameters

--- lib/simpleserver.rb
+++ lib/simpleserver.rb
@@ -1,5 +1,34 @@
 require "simpleserver/version"
+require 'socket'
 
 module Simpleserver
-  # Your code goes here...
+
+  def self.run
+    server = TCPServer.new 1337
+
+    loop do
+      client = server.accept
+      request = client.gets
+      verb, path, protocol = request.split(" ")
+      path = path.split('?')[0]
+      path = "." + path
+
+      if File.file?(path)
+        body = File.open(path, "r"){|file| file.read}
+        status = "200 OK"
+      else
+        body = "File not found\n"
+        status = "404 Not Found"
+      end
+
+      response = "#{protocol} #{status}\n"
+      response += "Content-Type: text/html\n"
+      response += "Content-Length: #{body.length}\n"
+      response += "Connection: close\n\n"
+      response += body
+
+      client.puts response
+      client.close
+    end
+  end
 end
